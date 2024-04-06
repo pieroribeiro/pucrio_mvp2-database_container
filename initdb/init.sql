06d145ea0405e3a59ebc04993487764f10f677de
Piero Ribeiro <piero.webmaster@gmail.com>
2024-04-06 16:28:54 -0300

    Final version

--- initdb/init.sql
+++ initdb/init.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS `cotacoes` (
     `symbol` VARCHAR(10) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
     `value` DECIMAL(10, 2) NOT NULL,
+    `variation` DECIMAL(10, 2) NOT NULL,
     `type` VARCHAR(20) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -30,10 +31,10 @@ CREATE TABLE IF NOT EXISTS `news` (
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-/*
-INSERT INTO apis (`name`, `symbol`, `url`, `api_key`, `load_symbols`, `active`) VALUES ('Exchange Rate API', 'coin', 'https://v6.exchangerate-api.com/v6/pair', '2130d4fcfb2c8196725dea41', 'USD|Dollar,EUR|Euro,CAD|Canadian Dollar', 1);
-INSERT INTO apis (`name`, `symbol`, `url`, `api_key`, `load_symbols`, `active`) VALUES ('Blockchain API', 'crypto', 'https://api.blockchain.com/v3/exchange/tickers', '', 'BTC-USD|Bitcoin,ETH-USD|Ethereum,SOL-USD|Solana', 1);
-*/
-
 INSERT INTO apis (`name`, `symbol`, `url`, `api_key`, `load_symbols`, `active`) VALUES ('News API', 'news', 'https://newsapi.org/v2/top-headlines', '47a44c3467c84467a0ccd7ae0db9ad9b', 'country=br&category=business&pageSize=10', 1);
-INSERT INTO apis (`name`, `symbol`, `url`, `api_key`, `load_symbols`, `active`) VALUES ('Awesome API', 'coin', 'https://economia.awesomeapi.com.br/json/last', '', 'BTC-BRL|crypto,ETH-BRL|crypto,ARS-BRL|coin,USD-BRL|coin,EUR-BRL|coin,CAD-BRL|coin', 1);
+INSERT INTO apis (`name`, `symbol`, `url`, `api_key`, `load_symbols`, `active`) VALUES ('Awesome API', 'coin', 'https://economia.awesomeapi.com.br/json/last', '', 'BTC-BRL|crypto,ETH-BRL|crypto,USD-BRL|coin,EUR-BRL|coin,GBP-EUR|coin,CAD-BRL|coin', 1);
+
+/*
+AWESOME APIs - Moedas Disponíveis
+https://economia.awesomeapi.com.br/xml/available
+*/
